96050b4ca6000a5d4016626bdf8fd0d8867bab12
Chen Ziying <150898614+czyzz00@users.noreply.github.com>
2026-02-25 19:38:32 +0800

    test

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-import { extension_settings } from "../../../extensions.js";
-
-const extensionName = "coc-universal-core";
-
-if (!extension_settings[extensionName]) {
-    extension_settings[extensionName] = {
-        characters: {}
-    };
-}
+alert("COC 扩展已加载");
 
 export async function onLoad() {
-    console.log("COC loaded");
-
-    addUI();
-}
-
-function addUI() {
-    const btn = document.createElement("button");
-    btn.textContent = "COC测试按钮";
-    btn.onclick = () => alert("扩展正常工作");
-    
-    document.body.appendChild(btn);
+    alert("COC onLoad 执行");
 }
