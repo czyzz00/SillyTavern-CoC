782f9dbef68de442b55dfeb4080de89cfd6e9587
Chen Ziying <150898614+czyzz00@users.noreply.github.com>
2026-02-25 20:41:59 +0800

    ui

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-console.log("COC legacy test running");
+import { extension_settings } from "../../../extensions.js";
 
-(function() {
+export async function onLoad() {
     const div = document.createElement("div");
     div.style.position = "fixed";
-    div.style.top = "20px";
-    div.style.left = "20px";
-    div.style.background = "red";
+    div.style.bottom = "20px";
+    div.style.right = "20px";
+    div.style.background = "black";
     div.style.color = "white";
     div.style.padding = "10px";
     div.style.zIndex = "9999";
-    div.textContent = "COC 旧式扩展运行中";
+    div.innerHTML = `
+        COC扩展运行中<br>
+        <button id="coc-btn">测试</button>
+    `;
+
     document.body.appendChild(div);
-})();
+
+    document
+        .getElementById("coc-btn")
+        ?.addEventListener("click", () => {
+            alert("UI 注入成功");
+        });
+}
